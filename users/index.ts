0fde690c9f83fa42410d109bb1ab2d52321a3e2e
Adam Firdaus <adamfir@pahamify.com>
2022-05-26 10:56:46 +0700

    feat: add users service

---
+++ users/index.ts
@@ -0,0 +1,58 @@
+import { ApolloServer, gql } from "apollo-server";
+import { buildSubgraphSchema } from "@apollo/subgraph";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// define User type
+interface User {
+  id: string;
+  username: string;
+}
+
+// static fetchUserById function
+const fetchUserById = (id: string): User => {
+  const user: User = {
+    id,
+    username: `John Doe ${id}`,
+  };
+  return user;
+} 
+
+const typeDefs = gql`
+  extend schema
+    @link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@shareable"])
+
+  type Query {
+    me: User
+  }
+
+  type User @key(fields: "id") {
+    id: ID!
+    username: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me() {
+      return fetchUserById("1")
+    }
+  },
+  User: {
+    __resolveReference(user: User){
+      return fetchUserById(user.id)
+    }
+  }
+};
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({
+    typeDefs,
+    resolvers,
+  })
+});
+
+server.listen(process.env.PORT || 8002).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
