d9aca78927a5fd38acb4d1cd0d4ec0726754eb06
Adam Firdaus <adamfir@pahamify.com>
2022-05-26 18:35:17 +0700

    fix: remove express from gateway

--- gateway/index.ts
+++ gateway/index.ts
@@ -1,30 +1,3 @@
-/**
- * express demonstration start
- */
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Express and TypeScript!!');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
-});
-
-/**
- * express demonstration end
- */
-
-/**
- * apollo gateway demonstration start
- */
-
 import { ApolloServer } from "apollo-server";
 import { ApolloGateway } from "@apollo/gateway";
 import { readFileSync } from "fs";
@@ -38,11 +11,8 @@ const server = new ApolloServer({
   gateway,
 });
 
-server.listen({ port: process.env.PORT_GATEWAY }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 8001 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 }).catch(e => {
   console.error(e);
 });
-/**
- * apollo gateway demonstration end
- */
--- readme.md
+++ readme.md
@@ -1,8 +1,7 @@
-# Express js + TypeScript + GraphQL Federation 2
+# Apollo GraphQL Federation 2 + TypeScript
 
-This repository contain a minimal demonstration of express js + typescript + graphql federation 2.
+This repository contain a minimal demonstration of apollo graphql federation 2 + typescript.
 
 References:
-- [Express + typescript](https://blog.logrocket.com/how-to-set-up-node-typescript-express/)
 - [About Apollo GraphQL Federation 2](https://www.apollographql.com/docs/federation/federation-2/new-in-federation-2/)
 - [Federation 2 Quickstart](https://www.apollographql.com/docs/federation/quickstart/setup/)
